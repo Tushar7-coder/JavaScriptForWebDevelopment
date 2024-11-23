8774e33e877a6ca151fcb46b8aa6cec135ab0456
Tusharkanta Bihari <72273347+Tushar7-coder@users.noreply.github.com>
2024-11-23 18:33:58 +0000

    Variable code

---
+++ 01-Basics/02_variable.js
@@ -0,0 +1,7 @@
+const accountNumber =2345;
+let accountPassword ="12345";
+var accountEmail = "tusharkanta@gmail.com"
+isLogIn = false;
+console.table([accountNumber,accountPassword,accountEmail,isLogIn])
+
+//prefer not to use var 
