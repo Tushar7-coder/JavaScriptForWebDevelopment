b13702b1d6c5b37467caf2a3db5411d5c01e4cad
Tusharkanta Bihari <72273347+Tushar7-coder@users.noreply.github.com>
2024-11-23 18:55:47 +0000

    DataType added

---
+++ 01-Basics/03_dataType.js
@@ -0,0 +1,7 @@
+"use strict"
+//number , big int ,string ,boolean,null (stand alone value),undefined ,symbool,
+
+
+console.log(typeof([]));
+console.log(typeof (null));
+console.log(typeof (undefined));
