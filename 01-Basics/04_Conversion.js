6f66b83642c5caab0f734734d9006f47156530dc
Tusharkanta Bihari <72273347+Tushar7-coder@users.noreply.github.com>
2024-11-24 15:53:48 +0000

    Type Conversion

---
+++ 01-Basics/04_Conversion.js
@@ -0,0 +1,42 @@
+let score =33;
+let score2 = "33";
+console.log(typeof score);
+console.log(typeof score2);
+let valueInNumber = Number (score)
+console.log(valueInNumber);
+console.log(typeof valueInNumber);
+const val ="33abc";
+console.log(val);
+const valInNumber = Number(val);
+console.log(typeof valInNumber);
+console.log(valInNumber);
+console.log(typeof NaN);
+let val2 = undefined;
+console.log(val2);
+console.log(typeof val2);
+let val3 = null;
+console.log("null value ",val3);
+console.log(Number(val3));
+console.log(" null type "+typeof val3);
+const val4 = true;
+console.log(val4);
+console.log(false);
+console.log(Number (true));
+console.log(Number (false));
+
+//Conversion to number 
+// "33" => 33
+// "33abc" => Nan
+// true => 1
+// false => 0
+
+//Boolean 
+
+// "" => false
+// "Tushar" => true
+// 0 => false
+// 1 => true
+
+//String
+
+//
