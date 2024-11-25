aa784b728ce2cb10026e345d6ff846cd1bce9470
Tusharkanta Bihari <72273347+Tushar7-coder@users.noreply.github.com>
2024-11-25 11:45:06 +0000

    Date Time

---
+++ 01-Basics/10_DateAndTime.js
@@ -0,0 +1,22 @@
+let myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleDateString());
+// console.log(typeof myDate);
+
+let myCreatedDate = new Date(2023,0,23,5,2)
+// console.log(myCreatedDate.toDateString())
+
+let newDate =new Date();
+// console.log(newDate.toLocaleDateString())
+
+
+let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+// console.log(newDate.getTime());
+//console.log(Math.round(Date.now()/1000))
+console.log(myDate.getMonth())
+
+
