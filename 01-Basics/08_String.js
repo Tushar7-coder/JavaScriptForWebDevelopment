950b673fa7890df97ce31a7f00bff7ca00f13ad7
Tusharkanta Bihari <72273347+Tushar7-coder@users.noreply.github.com>
2024-11-25 10:34:21 +0000

    string methods

---
+++ 01-Basics/08_String.js
@@ -0,0 +1,22 @@
+const name ="Tushar";
+const age =23;
+// console.log(`My name is ${name} . I am ${age} years old .`);
+const gameName =new String("       tushar-gta-com    ");
+// console.log(gameName);
+// console.log(gameName[0]);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('g'));
+// console.log(gameName.substring(2));
+// console.log(gameName.slice(2));
+// console.log(gameName.slice(2,5));
+// console.log(gameName.trim())
+
+// const url =" www.tweeter##.##gam#com";
+// console.log(url.replaceAll("#","-"))
+// console.log("tushar kanta bihari".split(" "));
+// console.log(url.includes("r"))
+
+
+
