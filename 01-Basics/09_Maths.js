7bfed9c20612dfdea9bc71b8310ca462d92b62b5
Tusharkanta Bihari <72273347+Tushar7-coder@users.noreply.github.com>
2024-11-25 11:12:27 +0000

    Math and Num

---
+++ 01-Basics/09_Maths.js
@@ -0,0 +1,32 @@
+
+//Number
+const score =400;
+const balance = new Number(100);
+// console.log(score);
+// console.log(balance);
+// console.log(balance.toString().length);
+const mark =100.2347
+// console.log(mark.toFixed(2));
+// console.log(mark.toPrecision(6));
+
+const hundreds = 10000000000000000;
+// console.log(hundreds.toLocaleString("en-IN"));
+// console.log(Number.MAX_SAFE_INTEGER);
+// console.log(Number.MAX_VALUE);
+// console.log(Number.MIN_VALUE);
+// console.log(Number.MIN_SAFE_INTEGER);
+
+
+//Math
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(2.7));
+// console.log(Math.round(2.3));
+// console.log(Math.floor(2.7));
+// console.log(Math.ceil(2.1));
+// console.log(Math.min(2,4,2,3,2,3,2,1,3));
+// console.log(Math.max(23,23,2,3,565,3,5,63,54,3,43,3,3,3));
+console.log(Math.floor(Math.random()*10 ) + 1);
+
+//Math.random() always gives value between 0-1
