dde13dc20e5a78b5884012546c22f10caeb9146f
Tusharkanta Bihari <72273347+Tushar7-coder@users.noreply.github.com>
2024-11-25 06:08:04 +0000

    Operation in javaScript

---
+++ 01-Basics/05_Operstion.js
@@ -0,0 +1,20 @@
+const number = 5;
+const negativeNumber = -number;
+//console.log(negativeNumber);
+// console.log(2+2);
+// console.log(4-1);
+// console.log(2*1);
+// console.log(3/2);
+// console.log(3%2);
+
+const name1 = "Hello";
+const name2 = " Tushar"
+// console.log( name1 + name2);
+
+console.log();
+
+
+console.log(1 + "2");
+console.log("1" + 2);
+console.log(1 + 2 + "2");
+console.log("2" + 1);
