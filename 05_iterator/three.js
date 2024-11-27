92558c41094d385588242b6696e97beaecfaf16e
Tusharkanta Bihari <72273347+Tushar7-coder@users.noreply.github.com>
2024-11-27 07:58:28 +0000

    Map

---
+++ 05_iterator/three.js
@@ -0,0 +1,35 @@
+//for of
+//["","",""]
+//[{},{},{}]
+
+const arr = [1,2,3,4,5];
+// for(const num of arr){
+//     console.log(num)
+// }
+
+const greetings = "Hello world";
+// for(let greet of greetings){
+//     console.log(greet)
+// }
+
+const map = new Map();
+map.set("IN","India")
+map.set("USA","United states of America")
+map.set("Fr","France");
+map.set("IN","India");
+//console.log(map) //no duplicasy and order as input
+
+// for(const [key,value] of map){
+//     console.log(key,"-",value)
+// }
+
+const obj1 ={
+    "name" : "Tushar",
+    age :23
+}
+// for(const {key,valye} of obj1){
+//     console.log(key,"-",valye);
+// }
+//not iterable 
+
+
