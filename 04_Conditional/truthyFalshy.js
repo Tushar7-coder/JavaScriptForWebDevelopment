7996b172e866aeec941108e4128bbce09db4a3d7
Tusharkanta Bihari <72273347+Tushar7-coder@users.noreply.github.com>
2024-11-27 06:37:17 +0000

    truthy falshy values

---
+++ 04_Conditional/truthyFalshy.js
@@ -0,0 +1,39 @@
+const userEmail = "";
+// if(userEmail){
+//     console.log("email exists");
+
+// }
+// else{
+//     console.log("email not exists");
+// }
+
+//falsy values 
+// false,0,-0, BigInt 0n, "",null,undefined,NaN
+
+//truthy values
+//true," ",[],{},"0","false",function()[]
+
+// if(userEmail.length === 0){
+//     console.log("Email is empty");
+// }
+
+const empty ={};
+// if(Object.keys(empty).length === 0){
+//     console.log("Object is empty")
+// }
+
+//nullish coalescing  operator (??) : null Undefined
+let val1 ;
+// // val1 = 5??10
+// val1 = null ?? 10
+// val1 = undefined ?? 100
+// val1 = null ?? 3 ?? 4
+//console.log(val1);
+
+
+
+
+//ternary operator
+age =19;
+age < 18 ? console.log("Below 18") : console.log("18 +");
+
