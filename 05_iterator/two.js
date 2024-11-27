f6a04169262634506380ef7f40db3ce538da325c
Tusharkanta Bihari <72273347+Tushar7-coder@users.noreply.github.com>
2024-11-27 07:28:03 +0000

    while dowhile

---
+++ 05_iterator/two.js
@@ -0,0 +1,12 @@
+let index =0;
+// while(index <=10){
+//     console.log(index);
+//     index+= 2;
+// }
+
+let array = ["one","two","three"];
+let newArr =0;
+while (array.length >= newArr) {
+   newArr +=1;
+   console.log(newArr)
+}
