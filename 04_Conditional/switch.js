ae35351b99abfcdfcfbdc786860d189f46b78d68
Tusharkanta Bihari <72273347+Tushar7-coder@users.noreply.github.com>
2024-11-27 06:22:05 +0000

    switch added

---
+++ 04_Conditional/switch.js
@@ -0,0 +1,35 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+const day = 5
+switch(day){
+    case 0:
+        console.log("sunday");
+        break;
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("wednesday");
+        break;
+    case 4:
+        console.log("thursday");
+        break;
+    case 5:
+        console.log("friday");
+        break;
+    case 6:
+        console.log("saturday")            
+    default:
+        console.log("Enter valid number")                
+}
